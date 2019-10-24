95cec950ef1a8263b58c759501435cc0f04f94bc
minhoyun <minhoyun.dev@gmail.com>
2019-10-24 12:19:54 +0900

    Add fields to carts model

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core import models as core_models
+from users import models as user_models
 
-# Create your models here.
+
+class Cart(core_models.TimeStampedModel):
+
+    """ カートのモデルを定義する """
+
+    user = models.ForeignKey(
+        user_models.User, null=True, blank=True, on_delete=models.CASCADE
+    )
+    # products    = models.ManyToManyField(Product, blank=True)
+    subtotal = models.IntegerField(default=0)
+    total = models.IntegerField(default=0)
+
