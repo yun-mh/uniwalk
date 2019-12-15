7989969d4a0b07534c96c5e20bcba7875ab49089
minhoyun <minhoyun.dev@gmail.com>
2019-12-15 23:28:47 +0900

    Decorate cart page

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Cart)
+class CartAdmin(admin.ModelAdmin):
+
+    """ アドミンにカートテーブルを定義する """
+
+    pass
