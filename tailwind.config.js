1c6a83796ac6c209cfc5427b65e4e6808a84714b
cristhofer-r <cristhofer_rt@yahoo.co.jp>
2020-02-02 15:31:45 +0900

    !design!

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,12 +51,7 @@ module.exports = {
     boxShadow: {
       dark:
         " 0 4px 6px -1px rgba(0, 0, 0, .2), 0 2px 4px -1px rgba(0, 0, 0, .08)",
-<<<<<<< HEAD
-      default: 
-        "0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)",
-=======
       default: "0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px 0 rgba(0, 0, 0, .06)",
->>>>>>> master
       md:
         " 0 4px 6px -1px rgba(0, 0, 0, .1), 0 2px 4px -1px rgba(0, 0, 0, .06)",
       lg:
