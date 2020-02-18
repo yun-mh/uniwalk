89b1bfc0d520047fd17121a3268353c2f2194ebd
Minho Yun <minhoyun.dev@gmail.com>
2020-02-18 14:01:43 +0900

    Update translations

--- designs/apps.py
+++ designs/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+import vinaigrette
 
 
 class DesignsConfig(AppConfig):
-    name = 'designs'
+    name = "designs"
+
+    def ready(self):
+        Ingredient = self.get_model("Material")
+        vinaigrette.register(Ingredient, ["name"])
